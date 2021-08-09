1a1949cd7c54509edb0d50b093bbcda820849243
Vijay-Ky <vijayky007@gmail.com>
2021-08-09 19:17:58 +0530

    update

---
+++ WorkSpace.py
@@ -0,0 +1,30 @@
+
+'''
+n = len(str1)
+i = 0
+print("string in Forward Direction ---->")
+while i < n:
+	print(str1[i], end='')
+	i += 1
+i = n-1
+print()
+print("string in Backward Direction <----")
+while i >= 0:
+	print(str1[i], end='')
+	i -= 1
+'''
+'''
+for i in range(len(str1)):
+    print("The character at %d index position = %c" %(i, str1[i]))
+
+'''
+x ="anand"
+i = len(x)
+for i in (reversed(range(5))):
+    print(x[i], end='')
+        
+    
+    
+    
+    
+    
